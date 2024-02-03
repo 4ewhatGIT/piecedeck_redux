b247eea120c9f9c20a06374e88105733d00f39dc
4ewhatGIT <4ewhat@gmail.com>
2024-02-03 20:22:35 +1000

    PieceDeck redux alpha 0.0.1

---
+++ PieceDeck redux.py
@@ -0,0 +1,4 @@
+from sources import main
+
+if __name__ == '__main__':
+    main.main()
---
+++ sources/main.py
@@ -0,0 +1,37 @@
+import json
+from random import randint as rint
+from time import sleep
+
+CONFIG = "config.json"
+
+
+def initialize(config: str) -> dict:
+    with open(config, "r") as file:
+        config = json.load(file)
+    return config
+
+
+def title_screen(PD_NAME, VERSION) -> None:
+    print("Loading", end='')
+    i = 0
+    target = 3
+    while i < target:
+        if rint(0, 2) == 0:
+            i += 1
+            print(".", end='')
+            sleep(rint(2, 10) / 10)
+        sleep(0.2)
+    print()
+    print(f"Welcome to {PD_NAME} version {VERSION}!")
+
+
+def main() -> None:
+    config = initialize(CONFIG)
+    PD_NAME = config["PD_NAME"]
+    dev_mode = config["dev_mode"]
+    VERSION = config["VERSION"]
+    if dev_mode:
+        print(f"< {PD_NAME} config:")
+        for k in config.keys():
+            print(f"<< {k} = {config[k]}")
+    title_screen(PD_NAME, VERSION)
